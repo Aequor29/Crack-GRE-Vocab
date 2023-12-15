710580107a7ee95faf813257b5e717a2110072b8
Richard Hu <neverlight.hu@gmail.com>
2023-12-14 22:48:30 -0500

    make backend production ready

--- crackGreVocab/crackGreVocab/settings.py
+++ crackGreVocab/crackGreVocab/settings.py
@@ -9,25 +9,29 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+import os
+import dj_database_url
 
 from datetime import timedelta
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-964d1vj=&!-@j79g7%r78&j8z^@wizqw194yz%40^da*0!&_!b'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['127.0.0.1','localhost']
+ALLOWED_HOSTS += os.environ.get('ALLOWED_HOSTS').split()
 
 # Application definition
 
@@ -81,13 +85,9 @@ WSGI_APPLICATION = 'crackGreVocab.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'gre_vocab_db',
-        'USER': 'root',
-        'HOST': 'localhost',
-        'PORT': '3306',
-    }
+    'default': dj_database_url.config(
+        default="sqlite:///db.sqlite3" +os.path.join(BASE_DIR, 'db.sqlite3')
+    )
 }
 
 
